041743d8d5074e88ec1debe0fbf6c8ba3f82a345
Lucas <166824755+luscassas@users.noreply.github.com>
2024-05-13 18:49:33 -0300

    Add files via upload

---
+++ lista.segunda.un.vet.2.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  int n1, n2, n3, n4, n5, n6;
+
+  cout << "insira seis números inteiros: ";
+  cin >> n1 >> n2 >> n3 >> n4 >> n5 >> n6;
+
+  cout << "os números digitados foram: " << endl << n1 << endl << n2 << endl << n3 << endl << n4 << endl << n5 << endl << n6;
+  
+    return 0;
+  
+}
