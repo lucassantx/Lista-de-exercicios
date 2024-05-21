a560bc12f76843bcc9ad25c9a54eff17da8dd39e
Lucas <166824755+luscassas@users.noreply.github.com>
2024-05-21 20:35:00 -0300

    Create lista.segunda.un.vet.9.cpp

---
+++ lista.segunda.un.vet.9.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    int vetor[6];
+
+    cout << "Digite 6 valores inteiros pares: " << endl;
+    for (int i = 0; i < 6; ++i) {
+        cin >> vetor[i];
+    }
+if (!(vetor[0] % 2 == 0)) { 
+  cout << "Insira apenas valores pares!" << endl;
+  return 0;
+  
+  }
+  
+    cout << "Valores: " << endl;
+    for (int i = 0; i < 6; ++i) {
+        cout << "Posição " << i << ": " << vetor[i] << endl;
+    }
+
+    for (int i = 0; i < 6 / 2; ++i) {
+        int in = vetor[i];
+        vetor[i] = vetor[6 - 1 - i]; 
+        vetor[6 - 1 - i] = in;
+    }
+
+    cout << "Valores invertidos:" << endl;
+    for (int i = 0; i < 6; ++i) {
+        cout << "Posição " << i << ": " << vetor[i] << endl;
+    }
+
+    return 0;
+}
